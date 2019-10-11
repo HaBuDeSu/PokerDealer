ef3db310c176ce76086f856f4cad80f126dc299e
Ken Ruf <ruf.kenichi@gmail.com>
2019-10-10 21:29:29 -0500

    setup

---
+++ pokerdealer/src/components/Table.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function Table() {
+  return (
+    <div className="Table">
+      <div>
+          <img src="../Table.png"/>
+      </div>
+    </div>
+  );
+}
+
+export default Table;
